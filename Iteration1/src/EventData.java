b6552712b26cb5310a7a41ea2d35ec31d3709c37
unknown <jeongwonkim3@cmail.carleton.ca>
2021-02-04 09:40:06 -0500

    Added Floor thread class

--- Iteration1/src/EventData.java
+++ Iteration1/src/EventData.java
@@ -1,22 +1,25 @@
+import java.util.Date;
 
 public class EventData {
-	public long timestamp;
+	public Date timestamp;
 	public int floorNum;
-	public boolean floorButton; // True=Up, False = Down
+	public boolean upButton; // True=Up, False = Down
+	public boolean downButton;
 	public int carButton;
-	public boolean destination; // True=Event being sent to scheduler, False=Event being sent from scheduler
+	public int destination; // True=Event being sent to scheduler, False=Event being sent from scheduler
 	
 	
 	// Constructor for floor events
-	public EventData(long timestamp, int floorNum, boolean floorButton, boolean destination) {
+	public EventData(Date timestamp, int floorNum, boolean upButton, boolean downButton, int destination) {
 		this.timestamp = timestamp;
 		this.floorNum = floorNum;
+		this.upButton = upButton;
+		this.downButton = downButton;
 		this.destination = destination;
 		// this.carButton = -1;
-		this.floorButton = floorButton;
 	}
 	// Constructor for elevator events
-	public EventData(long timestamp, int floorNum, int carButton, boolean destination) {
+	public EventData(Date timestamp, int floorNum, int carButton, int destination) {
 		this.timestamp = timestamp;
 		this.floorNum = floorNum;
 		this.destination = destination;
---
+++ Iteration1/src/Floor.java
@@ -0,0 +1,80 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * This class represent a floor thread
+ * 
+ * @author Jeong Won Kim
+ *
+ */
+public class Floor implements Runnable {
+	private static Scheduler scheduler;
+	private static int floorNum;
+    private boolean UP_BUTTON = false;
+    private boolean DOWN_BUTTON = false;
+    
+    public Floor(Scheduler scheduler, int floorNum) {
+    	this.scheduler = scheduler;
+    	this.floorNum = floorNum;
+    }
+	
+    public static String readEventFromTextFile(String filename) {
+    	String rawData = "";
+        try {
+        	File file = new File(filename);
+            Scanner reader = new Scanner(file);
+            rawData = reader.nextLine();
+            System.out.println(rawData);
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist!");
+            e.printStackTrace();
+        }
+        return rawData;
+    }
+
+    public EventData convertTextEvent(String rawData) {
+    	String[] eInfo = rawData.split(" ");
+    	DateFormat dateFormat = new SimpleDateFormat("hh:mm:ss.mmm");
+    	Date timeStamp = dateFormat.parse(eInfo[0]);
+    	int floorNum = Integer.parseInt(eInfo[1]);
+    	if (eInfo[2].equals("Up"))
+    		UP_BUTTON = true;
+    	if (eInfo[2].equals("Down"))
+    		DOWN_BUTTON = true;
+    	int destination = Integer.parseInt(eInfo[3]);
+    	
+    	EventData eData = new EventData(timeStamp, floorNum, UP_BUTTON, DOWN_BUTTON, destination);
+    	return eData;
+    }
+    
+    public void sendEventToScheduler() {
+    	
+    }
+
+    public void readFromScheduler() {
+    	
+    }
+    
+	public void run() {
+        while(true){
+            System.out.println(Thread.currentThread().getName() + " took an event");
+            //scheduler.writeToFloor();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {}
+        }
+	}
+
+	/**
+	public static void main(String[] args) {
+
+	}
+	**/
+}
