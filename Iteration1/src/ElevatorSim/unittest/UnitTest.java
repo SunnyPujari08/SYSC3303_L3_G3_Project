06f5ee6fea3fbb337220423dd34d6f6254c04719
unknown <jeongwonkim3@cmail.carleton.ca>
2021-02-06 12:55:37 -0500

    Added unittest directory

---
+++ Iteration1/src/ElevatorSim/unittest/UnitTest.java
@@ -0,0 +1,18 @@
+package ElevatorSim.unittest;
+
+
+import junit.framework.TestCase;
+
+public class UnitTest extends TestCase {
+	
+	
+	public void testFileRead() {
+		
+	}
+
+	public void testConvertedEvent() {
+		
+	}
+	
+	
+}
