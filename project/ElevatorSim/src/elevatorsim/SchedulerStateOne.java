fd144abb1bce4360b72c0e50b7895677f15f06a7
unknown <jeongwonkim3@cmail.carleton.ca>
2021-02-27 18:40:45 -0500

    Implemented states for scheduler

---
+++ project/ElevatorSim/src/elevatorsim/SchedulerStateOne.java
@@ -0,0 +1,33 @@
+package elevatorsim;
+
+import elevatorsim.elevator.Elevator;
+
+public class SchedulerStateOne extends SchedulerState {
+
+	public SchedulerStateOne(Elevator elevator, Scheduler scheduler) {
+		super(elevator, scheduler);
+	}
+
+	@Override
+	public int handleEvent(EventData event) {
+		if(event.eventType == EventType.ELEVATOR_ARR_FLOOR_DOWN) {
+			scheduler.sendResponseToFloor(event.floorNum);
+			return Constants.SCHEDULER_STATE_THREE;
+		}
+		
+		return Constants.SCHEDULER_STATE_ONE;
+	}
+
+	@Override
+	public void entranceActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exitActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ project/ElevatorSim/src/elevatorsim/SchedulerStateTwo.java
@@ -0,0 +1,33 @@
+package elevatorsim;
+
+import elevatorsim.elevator.Elevator;
+
+public class SchedulerStateTwo extends SchedulerState {
+
+	public SchedulerStateTwo(Elevator elevator, Scheduler scheduler) {
+		super(elevator, scheduler);
+	}
+
+	@Override
+	public int handleEvent(EventData event) {
+		if(event.eventType == EventType.ELEVATOR_ARR_FLOOR_UP) {
+			scheduler.sendResponseToFloor(event.floorNum);
+			return Constants.SCHEDULER_STATE_FOUR;
+		}
+		
+		return Constants.SCHEDULER_STATE_TWO;
+	}
+
+	@Override
+	public void entranceActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void exitActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
